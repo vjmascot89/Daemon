f6f41c40ddc77abb16c4d92733372f8f000e17f8
vjmascot89 <jaiswal.vijay1989@gmail.com>
2015-10-25 20:04:08 +0530

    [Removed imports]

--- src/com/exec/module/UserSignUpRequest.java
+++ src/com/exec/module/UserSignUpRequest.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
